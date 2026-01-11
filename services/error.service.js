83e321af34e020acec1d3bd626df3025778bba06
maiychrus25 <ninhem575@gmail.com>
2026-01-11 20:04:11 +0700

    feat: errors during development or production!

--- services/error.service.js
+++ services/error.service.js
@@ -1,11 +1,38 @@
 const httpStatus = require('http-status');
 
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    err: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client!
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+    // Programming or other unknown error: don't leak error details to client!
+  } else {
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      status: 'error',
+      message: 'Something went very wrong!',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.status = err.status || 'error';
   err.statusCode = err.statusCode || httpStatus.INTERNAL_SERVER_ERROR;
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
