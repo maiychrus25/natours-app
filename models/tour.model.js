312a5db9995a2881ba84c5949a24efd4f46887b5
maiychrus25 <ninhem575@gmail.com>
2026-01-11 07:42:31 +0700

    feat: query middleware in mongoose!

--- models/tour.model.js
+++ models/tour.model.js
@@ -125,7 +125,7 @@ tourSchema.pre(/^find/, function (next) {
   next();
 });
 
-tourSchema.post('find', function (docs, next) {
+tourSchema.post(/^find/, function (docs, next) {
   console.log(`Query took ${Date.now() - this.start} miliseconds!`);
   next();
 });
