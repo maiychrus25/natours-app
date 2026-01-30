cf46c53919a6a756df2d79bff91b764349e909aa
maiychrus25 <ninhem575@gmail.com>
2026-01-30 21:23:20 +0700

    feat(views): build the tour overview page - Implement tour overview layout - Render dynamic tour data in pug templates

--- controllers/views.controller.js
+++ controllers/views.controller.js
@@ -1,10 +1,15 @@
 const httpStatus = require('http-status');
+const tourService = require('../services/tour.service');
+const catchAsync = require('../utils/catchAsync');
 
-exports.renderOverview = (req, res) => {
+exports.renderOverview = catchAsync(async (req, res, next) => {
+  const tours = await tourService.getTours({ ...req.query, limit: 10 });
+
   res.status(httpStatus.OK).render('overview', {
-    title: 'All tours'
+    title: 'All tours',
+    tours: tours
   });
-};
+});
 
 exports.renderTour = (req, res) => {
   res.status(httpStatus.OK).render('tour', {
