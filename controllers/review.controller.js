fc869c3ef7a70f60b002078091d51ffaae3fd43c
maiychrus25 <ninhem575@gmail.com>
2026-01-30 16:50:44 +0700

    about: resolve merge conflict between main and refactor/mvc-service-layer!

--- controllers/review.controller.js
+++ controllers/review.controller.js
@@ -1,59 +1,11 @@
 const reviewService = require('../services/review.service');
 const handlerFactory = require('./handlerFactory.controller');
 
-<<<<<<< HEAD
-exports.getReviews = catchAsync(async (req, res, next) => {
-  const reviews = await reviewService.getReviews(req.user.id, req.params.tourId);
-
-  res.status(httpStatus.OK).json({
-    status: 'success',
-    results: reviews.length,
-    data: {
-      reviews: reviews,
-    }
-  })
-});
-
-exports.getReview = catchAsync(async (req, res, next) => {
-  const review = await reviewService.getReview(req.user.id, req.params.tourId, req.params.reviewId);
-  
-  res.status(httpStatus.OK).json({
-    status: 'success',
-    data: {
-      review: review,
-    }
-  });
-});
-
-exports.createReview = catchAsync(async (req, res, next) => {
-  let tourId = req.body.tour;
-  let userId = req.body.user;
-  if (!userId) userId = req.user.id;
-  if (!tourId) tourId = req.params.tourId;
-
-  const data = {
-    review: req.body.review,
-    rating: req.body.rating,
-    tour: tourId,
-    user: userId,
-  }
-
-  const newReview = await reviewService.createReview(data);
-  
-  res.status(httpStatus.CREATED).json({
-    status: 'success',
-    data: {
-      review: newReview,
-    }
-  });
-});
-=======
 exports.setUserTourIds = (req, res, next) => {
   if (!req.body.user) req.body.user = req.user.id;
   if (!req.body.tour) req.body.tour = req.params.tourId;
   next();
 }
->>>>>>> refactor/mvc-service-layer
 
 exports.getReviews = handlerFactory.getAll(reviewService.getReviews); 
 exports.getReview = handlerFactory.getOne(reviewService.getReview);
--- services/review.service.js
+++ services/review.service.js
@@ -1,30 +1,8 @@
 const Review = require('../models/review.model');
 const baseService = require('./base.service');
 
-<<<<<<< HEAD
-exports.getReviews = async (tourId, userId) => {
-  const reviews = await Review.find({ user: userId, tour: tourId }); 
-  return reviews;
-}
-
-exports.getReview = async (userId, tourId, reviewId) => {
-  const review = await Review.findOne({ user: userId, tour: tourId, _id: reviewId })
-  if (!review) {
-    throw new AppError('Can not find review with that ID!', httpStatus.NOT_FOUND);
-  }
-
-  return review;
-}
-
-exports.createReview = async (data) => {
-  const newReview = await Review.create(data);
-  return newReview;
-}
-
-=======
 exports.getReviews = baseService.getAll(Review); 
 exports.getReview = baseService.getOne(Review);
 exports.createReview = baseService.createOne(Review); 
 exports.updateReview = baseService.updateOne(Review);
->>>>>>> refactor/mvc-service-layer
 exports.deleteReview = baseService.deleteById(Review);
